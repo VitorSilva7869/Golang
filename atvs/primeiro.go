436a2c2547d48eb3671294d5bd6f6968ffca63e9
Vitor Silva <99691815+VitorSilva7869@users.noreply.github.com>
2024-07-10 16:48:45 -0300

    Entrada de dados

--- atvs/primeiro.go
+++ atvs/primeiro.go
@@ -2,6 +2,15 @@ package main
 
 import "fmt"
 
-func main(){
-	fmt.Println("Fala, galera")
+func main() {
+	var nome string
+	fmt.Print("Qual o nome do usuario: ")
+	fmt.Scanf("%s", &nome)
+	fmt.Printf("O nome do usuario é %s!\n", nome) 
+
+	texto := "Qual o nome da serie: " 
+	fmt.Print(texto)
+	var serie string
+	fmt.Scanf("%s", &serie)
+	fmt.Printf("O nome da serie é %s\n", serie) 
 }
