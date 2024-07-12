cd37f244c0a35dabd53703d6b725e05e91d3f548
Vitor Silva <99691815+VitorSilva7869@users.noreply.github.com>
2024-07-11 21:16:02 -0300

    Formatando strings

--- atvs/primeiro.go
+++ atvs/primeiro.go
@@ -1,16 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
-	var nome string
-	fmt.Print("Qual o nome do usuario: ")
-	fmt.Scanf("%s", &nome)
-	fmt.Printf("O nome do usuario é %s!\n", nome) 
+	frase := "   OLA mundo!    "
+	primeiroCaracter := frase[0]//imprimir uma carcter
+	fmt.Printf("Primeira caracter: %c", primeiroCaracter)
+	segundaFrase := frase[4:9]//imprimir uma quantidade de caracter
+	fmt.Printf("\nSegunda frase: %s", segundaFrase)
+	frasesminusculo := strings.ToLower(frase)//Converter para minusculo
+	fmt.Printf("\nFrase minusculo: %s", frasesminusculo)
+	fraseMaiusculo := strings.ToUpper(frase)//Converter para maiusculo
+	fmt.Printf("\nFrase Maiusculo: %s", fraseMaiusculo)
+	tirarEspaços := strings.Trim(frase, " ")
+	fmt.Printf("\nTirando Espaços: %s", tirarEspaços)
+	quantasCaracter := len(frase)//ler quanto de espaço tem ultilozado
+	fmt.Printf("\nQuantos Espaços tem: %d", quantasCaracter)
 
-	texto := "Qual o nome da serie: " 
-	fmt.Print(texto)
-	var serie string
-	fmt.Scanf("%s", &serie)
-	fmt.Printf("O nome da serie é %s\n", serie) 
 }
